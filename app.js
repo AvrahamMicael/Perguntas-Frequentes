87be609210a9a1fa345aac351b16664be5cdf378
AvrahamMicael <avrahammicael@protonmail.com>
2022-03-23 17:10:03 -0300

    apenas 1 pergunta aberta

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-const btns = document.querySelectorAll('.question-btn')
+const perguntas = document.querySelectorAll('.question')
 
-btns.forEach( btn => {
-    btn.addEventListener('click', b => {
-        const pergunta = b.currentTarget.parentElement.parentElement
+perguntas.forEach(pergunta => {
+    const botao = pergunta.querySelector('.question-btn')
+    botao.addEventListener('click', () => {
         pergunta.classList.toggle('show-text')
+
+        perguntas.forEach(pergunta2 => {
+            if(pergunta2 !== pergunta) pergunta2.classList.remove('show-text')
+        })
     })
 })
-
-//------------------ ou
-
-// const perguntas = document.querySelectorAll('.question')
-
-// perguntas.forEach(pergunta => {
-//     const botao = pergunta.querySelector('.question-btn')
-//     botao.addEventListener('click', () => {
-//         pergunta.classList.toggle('show-text')
-//     })
-// })
